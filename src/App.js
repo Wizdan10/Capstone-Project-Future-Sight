e7fc99c6da758a9d4001a30614334316eabe4924
Mochammad Wizdan Qutra tu'ain <76710531+Wizdan10@users.noreply.github.com>
2022-11-09 21:37:09 +0700

    Add App and Index.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainPage from "../src/views/pages/MainPage";
+import {BrowserRouter} from 'react-router-dom'
+function App() {
+  return(
+    <BrowserRouter>
+      <MainPage />
+      </BrowserRouter>
+  ) 
+}
+
+export default App;
