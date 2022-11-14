1904ce23399b3373999a75e436c9e5a0ecb84796
Mochammad Wizdan Qutra tu'ain <76710531+Wizdan10@users.noreply.github.com>
2022-11-14 20:16:05 +0700

    Update file NotFound
    Update file NotFound

--- src/views/pages/NotFound.js
+++ src/views/pages/NotFound.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function NotFound(){
     return(
-        <div className="not-found text-center">
+        <div className="not-found text-center mt-5">
             <h3 className="text-dark">404 Not Found</h3>
         </div>
     )
