14116f3185eec7d5c7d19c4a08163f604a08358a
Gilang Syafiul Anam <90433627+Gilang-sya@users.noreply.github.com>
2022-11-21 00:43:19 +0700

    update code

--- src/components/ProfesiItem.js
+++ src/components/ProfesiItem.js
@@ -1,34 +1,26 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+// import Card from 'react-bootstrap/Card';
+// import Col from 'react-bootstrap/Col';
+// import Row from 'react-bootstrap/Row';
 // import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 
 
 function ProfesiItem ({ id, job_title, imageProfesi, })
 {
-    return(
-        
+    return(       
         <div className="card-profesi">
-            <Row xs={1} sm={2} md={3}  >
-                {Array.from({ length: 6 }).map((_) => (
-                    <Col>
-                        <Card>
-                    <div className="profesi">
-                        <div className="image-profesi">
-                            <img src={imageProfesi} alt={job_title} />
-                        </div>
-                        <div className="title-job">
-                                    <p><Link to={`/Profesi/${id}`}>
+                        <div className="profesi">
+                            <div className="image-profesi">
+                                <img className="img-_" src={imageProfesi} alt={job_title} />
+                            </div>
+
+                            <div className="title-job">
+                                <Link to={`/profesi/${id}`}>
                                         {job_title}
-                                    </Link></p>
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-                        </Card>
-                </Col>
-                ))}
-            </Row>
         </div>
     )
 }
