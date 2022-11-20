5cf1f37a860bc3c6b240adf1b9da8f23337c0a91
Gilang Syafiul Anam <90433627+Gilang-sya@users.noreply.github.com>
2022-11-21 00:45:43 +0700

    add new file

---
+++ src/views/pages/LoginPage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropsTypes from "prop-types";
+
+import ImageMain from "../../images/icons/MainPage.png";
+import LoginTemplate from "../../components/LoginTemplate";
+import RegisterTemplate from "../../components/RegisterTemplate";
+function LoginPage({loginSuccess}) {
+  return (
+    <React.Fragment>
+    <div className="body-masthead">
+      <div className="masthead">
+        <div className="masthead-content text-dark">
+          <div className="container-fluid px-4 px-lg-0">
+            <h1 className="fst lh-1 ">Terangi masa depan</h1>
+            <h1 className="fst lh-1 mb-4 textColor">Tanpa salah pilihan</h1>
+            <p className="mb-5">
+              Membantu muda mudi dalam menentukan masa depan impian mereka tanpa
+              salah memilih
+            </p>
+            <LoginTemplate loginSuccess={loginSuccess}/>
+            <RegisterTemplate />
+          </div>
+        </div>
+      </div>
+      </div>
+      <img src={ImageMain} width={500} alt="MainPage" className="imgMain" />
+    </React.Fragment>
+  );
+}
+LoginPage.propTypes = {
+  loginSuccess: PropsTypes.func,
+};
+export default LoginPage;
---
+++ src/views/pages/NotFoundPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+ function NotFound404(){
+    return(
+        <section>
+            <div>
+            <h1><strong>404</strong></h1>
+            <p>Page Not NotFound404</p>
+            </div>
+        </section>
+    )
+}
+
+export default NotFound404
