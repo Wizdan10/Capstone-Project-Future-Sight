e8431af3a7d2252811e5c666567b96cbea0d3a1c
Gilang Syafiul Anam <90433627+Gilang-sya@users.noreply.github.com>
2022-11-21 00:44:40 +0700

    update code

--- src/views/pages/DetailPage.js
+++ src/views/pages/DetailPage.js
@@ -3,48 +3,30 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import DetailProfesi from "../../components/DetailProfesi";
 import { getProfesi } from "../../utils/dataset";
-import NavbarMain from "../../components/NavBar";
 
 
-function DetailPageWraper(){
-    const {id} = useParams();
-    return <DetailPage id = {id}/>
-}
 
-class DetailPage extends React.Component{
+class DetailPageWraper extends React.Component{
+
     constructor(props){
-        super(props)
-        this.state={
-            datas:''
-        }
-    }
+        super(props);
 
-    async componentDidMount(){
-        const {prof} = await getProfesi(this.props.id);
-        this.setState(()=>{
-            return {
-                datas: prof,
-            }
-        })
+        this.state = {
+            data: getProfesi(props.id)            
+        }
     }
     render(){
-        if (this.state.datas === 0){
-            return(
-                <div>
-                    <NavbarMain/>
-                    <p>Profession not found</p>
-                </div>
-            )
-        }
-
-        return (
-            <div>
-                <DetailProfesi {...this.state.datas}/>
-            </div>
+        return(
+            <DetailProfesi {...this.state.data} />
         )
     }
 }
 
+function DetailPageWrap(){
+    const {id} = useParams()
+    return <DetailPageWraper id= {id}/>
+}
+
 // tambahin propTypes disini
 
-export default DetailPageWraper;
+export default DetailPageWrap;
