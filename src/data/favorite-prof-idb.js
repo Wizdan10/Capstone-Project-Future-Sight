2b8ea3e5cf78a9223f27799f507e5a876d084cd5
Muhammad Faqih <99806357+FQH09@users.noreply.github.com>
2022-11-24 21:27:25 +0700

    Add files via upload

--- src/data/favorite-prof-idb.js
+++ src/data/favorite-prof-idb.js
@@ -1,29 +1,29 @@
 import { openDB } from 'idb';
-import CONFIG from '../globals/config';
+import CONFIG from '../global/config';
 
-const { DB_NAME, DB_VERSION, OBJECT_STORE_NAME } = CONFIG;
+const { DATABASE_NAME, DATABASE_VERSION, OBJECT_STORE_NAME } = CONFIG;
 
-const dbOpen = openDB(DB_NAME, DB_VERSION, {
+const dbOpen = openDB(DATABASE_NAME, DATABASE_VERSION, {
     upgrade(database) {
       database.createObjectStore(OBJECT_STORE_NAME, {
         keyPath: 'id',
       });
     },
-});
-
-const FavoriteProfDB = {
-    async getResto(id) {
+  });
+  
+const FavoriteProfDb = {
+    async getProf(id) {
       return (await dbOpen).get(OBJECT_STORE_NAME, id);
     },
-    async getAllResto() {
+    async getAllProf() {
       return (await dbOpen).getAll(OBJECT_STORE_NAME);
     },
-    async putResto(resto) {
-      return (await dbOpen).put(OBJECT_STORE_NAME, resto);
+    async putProf(prof) {
+      return (await dbOpen).put(OBJECT_STORE_NAME, prof);
     },
-    async deleteResto(id) {
+    async deleteProf(id) {
       return (await dbOpen).delete(OBJECT_STORE_NAME, id);
     },
-  };
+};
   
-  export default FavoriteProfDB;
+export default FavoriteProfDb;
