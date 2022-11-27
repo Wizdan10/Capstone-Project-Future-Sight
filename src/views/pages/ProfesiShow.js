ce2f53f0b6e4829436ca73e495d13c71fdee198b
Gilang Syafiul Anam <90433627+Gilang-sya@users.noreply.github.com>
2022-11-27 23:26:24 +0700

    add filter button

--- src/views/pages/ProfesiShow.js
+++ src/views/pages/ProfesiShow.js
@@ -3,6 +3,7 @@ import React from "react";
 import ProfesiCardList from "../../components/ProfesiCardList";
 import {getAllProfesi} from "../../utils/dataset";
 import SearchEngine from "../../components/SearchEngin";
+import FilterButton from "../../components/FilterButton";
 
 class ProfresiShow extends React.Component{
     constructor(props){
@@ -11,11 +12,14 @@ class ProfresiShow extends React.Component{
         this.state = {
             search: "",
             datas : getAllProfesi(),
+            filterBtn: getAllProfesi(),
+            
         };
         this.onSearchHandler = this.onSearchHandler.bind(this);
     }
 
     onSearchHandler = (event) =>{
+
         this.setState(()=>{
             return{
                 search: event.target.value
@@ -24,15 +28,30 @@ class ProfresiShow extends React.Component{
         
     }
 
+    onFilteringBtns = (e)=>{
+        let filtered;
+        console.log(e.target.value)
+        if(e.target.value === "All"){
+            filtered = this.state.datas;
+        }
+        else{
+            filtered = this.state.datas.filter(elm=>elm.company_industry===e.target.value)
+        }
+
+        this.setState({
+            filterBtn: filtered
+        })
+    }
+
     render(){
-        const datas = this.state.datas.filter(
+        const datas = this.state.filterBtn.filter(
             (data)=>data.job_title.toLowerCase().includes(
                 this.state.search.toLowerCase()))
 
         return(
-
             <div className="">
-                <SearchEngine keyword={this.state.search} keywordChange={this.onSearchHandler}/>
+                <FilterButton onFilteringBtns={this.onFilteringBtns} />
+                <SearchEngine keyword={this.state.search} keywordChange={this.onSearchHandler} />
                 <ProfesiCardList list = {datas}/>
             </div>
         )
